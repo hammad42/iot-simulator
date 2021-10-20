a79c1be62b09b3993bd9c544a9bf9f1372e7a576
Hammad Shamim <hammadshamim642@gmail.com>
2021-10-20 23:52:20 +0500

    working
    make seperate module for pubsub which is include in simulator

--- main.py
+++ main.py
@@ -2,39 +2,42 @@ from google.cloud import pubsub_v1
 import json
 import random
 import time
+from pubsub import publish_sensor as ps
 
-def simulator(stop_):
+def simulator():
     project_id = "elaborate-howl-285701"
     topic_id = "iot-check"
 
-    publisher = pubsub_v1.PublisherClient()
-    topic_path = publisher.topic_path(project_id, topic_id)
-    for x in range(0,stop_):
+    dictt_carbon_mono={'Air_Pollutant':'Carbon_monoxide','Useful_detection':random.triangular(0,0.3),'level':random.randint(0,50)}
+    dictt_sulpher_di={'Air_Pollutant':'sulpher_dioxide','Useful_detection':random.triangular(0,100),'level':random.randint(0,100)}
+    dictt_carbon_di={'Air_Pollutant':'carbon_dioxide','Useful_detection':random.triangular(350,600),'level':None}
+    dictt_nitrogen_di={'Air_Pollutant':'Nitrogen_dioxide','Useful_detection':random.triangular(0,50),'level':random.randint(0,100)}
+    dictt_Methane={'Air_Pollutant':'Methane','Useful_detection':random.triangular(1500,2000),'level':None}
+    dictt_Volatile={'Air_Pollutant':'Volatile_organic_compounds','Useful_detection':random.triangular(5,100),'level':None}
 
-        dictt_carbon_mono={'Air_Pollutant':'Carbon_monoxide','Useful_detection':random.triangular(0,0.3),'level':random.randint(0,50)}
-        dictt_sulpher_di={'Air_Pollutant':'sulpher_dioxide','Useful_detection':random.triangular(0,100),'level':random.randint(0,100)}
-        dictt_carbon_di={'Air_Pollutant':'carbon_dioxide','Useful_detection':random.triangular(350,600),'level':'None'}
-        dictt_nitrogen_di={'Air_Pollutant':'Nitrogen_dioxide','Useful_detection':random.triangular(0,50),'level':random.randint(0,100)}
-        dictt_Methane={'Air_Pollutant':'Methane','Useful_detection':random.triangular(1500,2000),'level':'None'}
-        dictt_Volatile={'Air_Pollutant':'Volatile_organic_compounds','Useful_detection':random.triangular(5,100),'level':'None'}
+    sensor_carbon_mono=json.dumps(dictt_carbon_mono)
+    ps(project_id,topic_id,sensor_carbon_mono)
 
-        sensor_json=json.dumps(dictt)
-        print(sensor_json)
-        print(type(sensor_json))
-    return sensor_json
+    sensor_sulpher_di=json.dumps(dictt_sulpher_di)
+    ps(project_id,topic_id,sensor_sulpher_di)
 
+    sensor_carbon_di=json.dumps(dictt_carbon_di)
+    ps(project_id,topic_id,sensor_carbon_di)
 
+    sensor_nitrogen_di=json.dumps(dictt_nitrogen_di)
+    ps(project_id,topic_id,sensor_nitrogen_di)
 
+    sensor_Methane=json.dumps(dictt_Methane)
+    ps(project_id,topic_id,sensor_Methane)
+
+    sensor_Volatile=json.dumps(dictt_Volatile)
+    ps(project_id,topic_id,sensor_Volatile)
+        
+    return 'status:200'
 
 while True:
-    data = simulator(1)
-    # Data must be a bytestring
-    data = data.encode("utf-8")
-    # Add two attributes, origin and username, to the message
-    future = publisher.publish(
-        topic_path, data, origin="python-sample", username="gcp"
-    )
-    print(future.result())
+    simulator()
+    time.sleep(5)
 
-    print(f"Published messages with custom attributes to {topic_path}.")
-    time.sleep(5) # Sleep for 5 seconds
+
+
--- pubsub.py
+++ pubsub.py
@@ -1,20 +1,27 @@
-from google.cloud import pubsub_v1
+def publish_sensor(project_id, topic_id,data,origin='python-sample',username='gcp'):
+    """"
+    description: send data to pubsub topic
+    arguments:
+        project_id (str): name of GCP project ID
+        topic_id (str): name of pubsub topic
+        data (str): data you want to upload
+        origin (str): name of origin it could be any string
+        username (str): name of username it could be any
 
 
-project_id = "elaborate-howl-285701"
-topic_id = "iot-check"
+    returns (str): topic path id
+    """
+    from google.cloud import pubsub_v1
 
-publisher = pubsub_v1.PublisherClient()
-topic_path = publisher.topic_path(project_id, topic_id)
-
-for n in range(1, 10):
-    data = f"Message number {n}"
-    # Data must be a bytestring
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path(project_id, topic_id)
     data = data.encode("utf-8")
-    # Add two attributes, origin and username, to the message
     future = publisher.publish(
-        topic_path, data, origin="python-sample", username="gcp"
+        topic_path, data, origin=origin, username=username
     )
     print(future.result())
 
-print(f"Published messages with custom attributes to {topic_path}.")
+
+    return "Published messages with custom attributes to {} ".format(topic_path) 
+if __name__ =='__main__':
+    publish_sensor("elaborate-howl-285701", "iot-check","hello2")
